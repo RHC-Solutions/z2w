fcaa059ea842d77d531e57f137c35ea9f5f41091
Roman <roman@rhcsolutions.com>
2025-11-15 13:11:18 +0000

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,10 +1,8 @@
 @echo off
 echo Starting Zendesk to Wasabi B2 Offloader...
 echo.
-pip install SQLAlchemy
-python.exe -m pip install --upgrade pip
-pip install python-dotenv
-pip install Flask
+pip install -r requirements.txt
+
 python main.py
 pause
 
