0dbb16a51cdc8cc82332c0b7d5cb11ef8e45db79
Roman <roman@rhcsolutions.com>
2025-11-16 01:38:50 +0000

    .

--- run.bat
+++ run.bat
@@ -1,8 +1,8 @@
 @echo off
 echo Starting Zendesk to Wasabi B2 Offloader...
 echo.
-pip install -r requirements.txt
-
+python -m pip install -r requirements.txt
+python -m pip install --upgrade pip
 python main.py
 pause
 
